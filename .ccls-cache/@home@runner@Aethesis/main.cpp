a22a444a7aad5143171874b5f3e588cb7bb8a7ca
Stephen <102384025+Steve4Space@users.noreply.github.com>
2023-04-05 14:30:39 +0000

    Initial Language, adds C++ dependencies

--- .ccls-cache/@home@runner@Aethesis/main.cpp
+++ .ccls-cache/@home@runner@Aethesis/main.cpp
@@ -1,5 +1,34 @@
-git#include <iostream>
+//language made for TimeBom game engine.
+//barely working, input and output done. This isn't meant to function, rather, to be a prototype of whats to come with how Aesthesis works. It will be designed to work with TimeBom
+#include <fstream>
+#include <iostream>
 
-int main() {
-  std::cout << "Hello World!\n";
+class commands {
+public:
+  void write(std::string val) {
+    std::cout << val;
+  }
+
+public:
+  void writeLine(std::string val) {
+    std::cout << val;
+    std::cout << "\n";
+  }
+
+public:
+  // Now returns the string that was input instead of outputting it.
+  std::string input(std::string ask) {
+    std::cout << ask;
+    std::string input;
+    std::cin >> input;
+    return input;
+  };
+};
+
+int main() { //testing
+  commands com;
+  std::string val = "Hello World!";
+  com.write(val);
+  std::string ask = "Hello World!";
+  com.input(ask);
 }
